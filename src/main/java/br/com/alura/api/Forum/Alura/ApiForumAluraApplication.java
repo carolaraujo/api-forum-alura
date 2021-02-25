fd98e72ded844649a96f0da8486cf76105b78878
Caroline Araujo <ca.araujosouza@gmail.com>
2021-02-24 21:17:47 -0300

    meu primeiro commit

---
+++ src/main/java/br/com/alura/api/Forum/Alura/ApiForumAluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.api.Forum.Alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForumAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForumAluraApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/alura/api/Forum/Alura/model/StatusTopico.java
@@ -0,0 +1,10 @@
+package br.com.alura.api.Forum.Alura.model;
+
+public enum StatusTopico {
+	
+	NAO_RESPONDIDO,
+	NAO_SOLUCIONADO,
+	SOLUCIONADO,
+	FECHADO;
+
+}
