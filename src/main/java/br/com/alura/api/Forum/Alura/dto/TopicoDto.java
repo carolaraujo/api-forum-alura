35f8437a23f352159b61271c706bd4f2d53a8125
Caroline Araujo <ca.araujosouza@gmail.com>
2021-03-04 13:40:46 -0300

    refatorando a api com spring security

--- src/main/java/br/com/alura/api/Forum/Alura/dto/TopicoDto.java
+++ src/main/java/br/com/alura/api/Forum/Alura/dto/TopicoDto.java
@@ -1,8 +1,8 @@
 package br.com.alura.api.Forum.Alura.dto;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
 
 import br.com.alura.api.Forum.Alura.model.Topico;
 
@@ -36,8 +36,9 @@ public class TopicoDto {
 		return dataCriacao;
 	}
 
-	public static List<TopicoDto> converter(List<Topico> topicos) {
-		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+	public static Page<TopicoDto> converter(Page<Topico> topicos) {
+		return topicos.map(TopicoDto::new);
+				
 	}
 	
 	
--- src/main/java/br/com/alura/api/Forum/Alura/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/api/Forum/Alura/repository/TopicoRepository.java
@@ -1,13 +1,14 @@
 package br.com.alura.api.Forum.Alura.repository;
 
-import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.api.Forum.Alura.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
 
-	List<Topico> findByCursoNome(String nomeCurso);
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 	
 }
