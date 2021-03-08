9df8e558d741b0cb8d2ab78020772f61e451fbbd
Caroline Araujo <ca.araujosouza@gmail.com>
2021-03-08 11:57:51 -0300

    criando token com JWT

--- src/main/java/br/com/alura/api/Forum/Alura/configuration/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/api/Forum/Alura/configuration/security/SecurityConfiguration.java
@@ -1,13 +1,16 @@
 package br.com.alura.api.Forum.Alura.configuration.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -16,6 +19,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	private AutenticacaoService autenticacaoService;
+	
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
 
 	// configuracoes de autenticação
 	// aqui usamos os algoritmos de hash
@@ -28,10 +37,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http.authorizeRequests()
-			.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+			.antMatchers(HttpMethod.GET, "/topicos").permitAll() // permitindo a url topicos
+			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll() // permitindo tudo em tópicos
+			.antMatchers(HttpMethod.POST, "/auth").permitAll() // permitindo a url auth
 			.anyRequest().authenticated()
-			.and().formLogin();
+			.and().csrf().disable() // csrf é um tipo de ataque hacker
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // política de criação de sessao
 		}
 		
 		//configuracao de recursos estáticos(css, js, imagens)
---
+++ src/main/java/br/com/alura/api/Forum/Alura/configuration/security/TokenService.java
@@ -0,0 +1,29 @@
+package br.com.alura.api.Forum.Alura.configuration.security;
+
+import java.sql.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.api.Forum.Alura.model.Usuario;
+import io.jsonwebtoken.Jwts;
+
+@Service
+public class TokenService {
+
+	@Value("${forum.jwt.expiration")
+	private String expiration;
+
+	@Value("${forum.jwt.secret")
+	private String secret;
+
+	public String gerarToken(Authentication authenticaion) {
+		Usuario logado = (Usuario) authenticaion.getPrincipal();
+		Date hoje = new Date(0);
+		Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+		return Jwts.builder().setIssuer("API do form da Alura").setSubject(logado.getId().toString()).setIssuedAt(hoje)
+				.setExpiration(dataExpiracao).signWith(io.jsonwebtoken.SignatureAlgorithm.HS256, secret).compact();
+	}
+
+}
